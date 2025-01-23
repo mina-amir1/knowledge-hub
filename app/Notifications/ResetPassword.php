83c67010665b804862fbb40e7f6938214e5c5dd3
Mina Amir <mina.amir@scandiweb.com>
2025-01-23 17:39:42 +0200

    add the forgot password feature

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Mail\ResetPasswordMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPassword extends Notification
+{
+    use Queueable;
+
+    public $token;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): ResetPasswordMail
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new ResetPasswordMail($url))->to($notifiable->email);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
